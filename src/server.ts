2313e651a1ad1ea1fee264ce4bf7c2e24bd02833
quangdn <quangdn@gemvietnam.com>
2022-06-04 19:22:25 +0700

    cleanup file stored after return

--- src/server.ts
+++ src/server.ts
@@ -12,25 +12,6 @@ import {filterImageFromURL, deleteLocalFiles, validateImageUrl} from './util/uti
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/filteredimage", async ( req, res ) => {
@@ -39,7 +20,9 @@ import {filterImageFromURL, deleteLocalFiles, validateImageUrl} from './util/uti
       console.log("image from url: " + imageUrl)
       filterImageFromURL(imageUrl).then(filteredpath => {
         console.log("filteredpath: " + filteredpath)
-        res.sendFile(filteredpath)
+        res.sendFile(filteredpath, function (err) {
+          deleteLocalFiles([filteredpath])
+        })
       })
     } else {
       res.status(400).json({status: 400, message: "imageUrl is not valid"})
